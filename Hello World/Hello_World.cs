aa544fc1e2c0e6c80ce6b20d14eebd1a56527c66
Abhiraj Krishna <abhiraj.krishna@thetrainline.com>
2016-06-30 12:47:16 +0530

    First comment

---
+++ Hello World/Hello_World.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    public class Hello_World
+    {
+
+        public static void main(string[] args)
+        {
+            Console.WriteLine(WriteMessage());
+        }
+
+        public static string WriteMessage()
+        {
+           return "Hello World" ;
+        }
+    }
+}
---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello_World;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Hello_World.Hello_World.WriteMessage());
+
+        }
+    }
+}
